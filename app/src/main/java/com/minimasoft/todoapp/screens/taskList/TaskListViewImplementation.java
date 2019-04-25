76ab5ca90eab00b55ddbeb62628c668c7ed84ba8
Habibur Rahman <hr_habib@hotmail.com>
2019-04-25 10:31:32 +0600

    Minor changes to task list view

--- app/src/main/java/com/minimasoft/todoapp/screens/taskList/TaskListViewImplementation.java
+++ app/src/main/java/com/minimasoft/todoapp/screens/taskList/TaskListViewImplementation.java
@@ -1,13 +1,23 @@
 package com.minimasoft.todoapp.screens.taskList;
 
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.minimasoft.todoapp.R;
 import com.minimasoft.todoapp.screens.common.views.BaseObservableViewMvc;
 
 public class TaskListViewImplementation extends BaseObservableViewMvc<TaskListView.Listener> implements TaskListView{
+    private EditText editAddTask;
+    private Button buttonAdd;
+    private RecyclerView mRecyclerView;
     public TaskListViewImplementation(LayoutInflater inflater, ViewGroup parent) {
         setRootView(inflater.inflate(R.layout.layout_task_list, parent, false));
+
+        editAddTask = findViewById(R.id.task_title_edit_text);
+        buttonAdd = findViewById(R.id.add_button);
+        mRecyclerView = findViewById(R.id.recyler_task_list);
     }
 }
